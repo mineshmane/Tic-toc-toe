00fb36000d164296a567e27162a623996fd94a31
mineshmane <mineshmane94@gmail.com>
2020-01-30 11:30:34 +0530

    Added:UC3_toss for play first done

--- shell.sh
+++ shell.sh
@@ -6,11 +6,23 @@ declare -a board
 #constants
 PLAYER=X;
 COMPUTER=O;
-
+TAIL=0;
+HEAD=1
 function initializingBoard(){
 
 	board=(0,0,0,0,0,0,0,0,0)
 
 }
 
+function toss(){
+	playFirst=$((RANDOM%2))
+	if [[ $playFirst == $HEAD ]]
+	then 
+		echo "PLAYER play first "
+	else
+		echo  "computer play first"
+	fi
+}
 initializingBoard
+toss
+echo $playFirst
